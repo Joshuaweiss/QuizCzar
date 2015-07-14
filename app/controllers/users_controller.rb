02401e03ec318e5306c4b6067857ab891fde9e3e
joshua weiss <joshuaweiss@mac.com>
2015-07-14 12:09:43 -0400

    add signup validation error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    fail
+    @user = User.new(user_params)
+    if (@user.save)
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:password,:email);
   end
 
 end
