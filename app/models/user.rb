82247c63ce9ced0229bb0354da4b0b41b030b5c3
joshua weiss <joshuaweiss@mac.com>
2015-07-14 10:45:37 -0400

    create user model

---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  name            :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#
+
+include BCrypt
+
+class User < ActiveRecord::Base
+  validates :email, :name, :password_digest, :session_token, presence: true
+  validates :email, :session_token, uniqueness: true
+  validates :password, length: {minimum: 6, maximum: 20, allow_nil: true}
+  before_validation :session_token_exists
+
+  attr_reader :password
+
+  def self.new_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def password=(password)
+    @password = password;
+    self.password_digest = Password.create(password);
+  end
+
+  def is_password?(password)
+    Password.new(self.password_digest).is_password?(password);
+  end
+
+  def session_token_exists
+    self.session_token ||= User.new_session_token
+  end
+
+
+end
