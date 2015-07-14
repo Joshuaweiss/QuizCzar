e8b38be4bd10c9520058979fd7b1d98946071b7a
joshua weiss <joshuaweiss@mac.com>
2015-07-14 19:31:02 -0400

    move navbar to backbone

--- app/assets/javascripts/quiz_czar.js
+++ app/assets/javascripts/quiz_czar.js
@@ -4,6 +4,16 @@ window.QuizCzar = {
   Views: {},
   Routers: {},
   initialize: function($rootEl) {
-    QuizCzar.Routers.Router({$rootEl: $rootEl});
+    $rootEl.empty();
+
+    ///fetch feeds
+    var navBar = new QuizCzar.Views.Navbar();
+    $rootEl.append(navBar.render().$el);
+
+    var contentView = $('<section id="content">');
+    $rootEl.append(contentView);
+
+    new QuizCzar.Routers.Router({$rootEl: contentView});
+    Backbone.history.start();
   }
 };
---
+++ app/assets/javascripts/routers/router.js
@@ -0,0 +1,11 @@
+QuizCzar.Routers.Router = Backbone.Router.extend({
+    initialize: function(options){
+      this.$rootEl = options.$rootEl;
+    },
+    routes: {
+      "" : "recentQuizzes"
+    },
+    recentQuizzes: function() {
+      
+    }
+});
