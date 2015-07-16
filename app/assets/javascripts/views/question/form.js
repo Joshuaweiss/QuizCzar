97539e650ff32f5af24ca564d636226016b57048
joshua weiss <joshuaweiss@mac.com>
2015-07-16 07:31:47 -0400

    style quiz new begin answer form

--- app/assets/javascripts/views/question/form.js
+++ app/assets/javascripts/views/question/form.js
@@ -3,6 +3,11 @@ QuizCzar.Views.QuestionForm = Backbone.View.extend({
   tagName: "form",
   className: "question-form",
   initialize: function() {
+    var answers = this.model.answers();
+    answers.each(function(answer) {
+      var answerView = new QuizCzar.View.AnswerForm({model: answer});
+      this.addSubview(".answers", answerView);
+    }.bind(this));
   },
   render: function() {
     this.$el.html(this.template({question: this.model}));
