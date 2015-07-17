18ecd8ecc9bca054cf2c2268d89efb85e5b602ef
joshauweiss <joshuaweiss@mac.com>
2015-07-17 15:15:37 -0400

    add quiz show bar

--- app/assets/javascripts/views/quiz/show.js
+++ app/assets/javascripts/views/quiz/show.js
@@ -1,5 +1,6 @@
 QuizCzar.Views.QuizShow = Backbone.View.extend({
   template: JST["quiz/show"],
+  className: "quiz-show-view",
   render: function() {
     this.$el.html(this.template({quiz: this.model}));
     return this;
