d1395b7f708cf6b117f5a647c7b2426abc009527
joshauweiss <joshuaweiss@mac.com>
2015-07-22 22:44:49 -0400

    fix bug where users without email register as not having unique emails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722185345) do
+ActiveRecord::Schema.define(version: 20150723024209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,7 @@ ActiveRecord::Schema.define(version: 20150722185345) do
     t.datetime "picture_updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
   add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
 
 end
